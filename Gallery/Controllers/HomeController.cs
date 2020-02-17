2ae539a488c2cbbf2fc506768c3afc8b1db33559
M1g1 <migi4ok@gmail.com>
2020-02-17 16:57:55 +0200

    Gallery.Core was created, into which it was transferred: services, EXIF ​​loading, configuration manager. Gallery.Core/bin and Gallery.Core/obj added to .gitignore.

--- Gallery/Controllers/HomeController.cs
+++ Gallery/Controllers/HomeController.cs
@@ -6,185 +6,16 @@ using System.Web;
 using System.Web.Mvc;
 using System.Windows.Media.Imaging;
 using System.Drawing;
-using System.Security.Cryptography;
-using System.Text;
-using System.Runtime.InteropServices;
-using System.Drawing.Imaging;
-using Gallery.Settings;
+using Gallery.Core;
 
 namespace Gallery.Controllers
 {
     public class HomeController : Controller
-    { 
-
-      
+    {
         private static string pathToSave = GalleryConfigurationManager.GetPathToSave();
         private static string imageType = GalleryConfigurationManager.GetAvailableImageTypes();
 
-
-
-        private static string title;
-        private static string manufacturer;
-        private static string modelOfCamera;
-        private static string fileSize;
-        private static string dateCreation;
-        private static string dateUpload; 
-        public static string Title { get => title;}
-        public static string Manufacturer { get => manufacturer;}
-        public static string ModelOfCamera { get => modelOfCamera;}
-        public static string FileSize { get => fileSize;}
-        public static string DateCreation { get => dateCreation;}
-        public static string DateUpload { get => dateUpload;}
-
-        //
-        // Hash-Function
-        // Input: String
-        // Otput: String with ShaHash
-        //
-        public static string ComputeSha256Hash(string rawData)
-        {
-            // Create a SHA256   
-            using (SHA256 sha256Hash = SHA256.Create())
-            {
-                // ComputeHash - returns byte array  
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
-
-                // Convert byte array to a string   
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    builder.Append(bytes[i].ToString("x2"));
-                }
-                return builder.ToString();
-            }
-        }
-
-        //
-        // check for equality of pictures
-        // Input: Bitmap1, Bitmap2
-        // Output:
-        //        true - is equal
-        //        false - isn't equal
-        //
-        public static bool CompareBitmapsFast(Bitmap bmp1, Bitmap bmp2)
-        {
-            if (bmp1 == null || bmp2 == null)
-                return false;
-            if (object.Equals(bmp1, bmp2))
-                return true;
-            if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
-                return false;
-
-            int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8);
-
-            bool result = true;
-            byte[] b1bytes = new byte[bytes];
-            byte[] b2bytes = new byte[bytes];
-
-            BitmapData bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width, bmp1.Height), ImageLockMode.ReadOnly, bmp1.PixelFormat);
-            BitmapData bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), ImageLockMode.ReadOnly, bmp2.PixelFormat);
-
-            Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
-            Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
-
-            for (int n = 0; n <= bytes - 1; n++)
-            {
-                if (b1bytes[n] != b2bytes[n])
-                {
-                    result = false;
-                    break;
-                }
-            }
-
-            bmp1.UnlockBits(bitmapData1);
-            bmp2.UnlockBits(bitmapData2);
-
-            return result;
-        }
-
-
-        public static void LoadExifData(string LoadExifPath)
-        {
-            try
-            {
-                FileInfo fileInfo = new FileInfo(LoadExifPath);
-               
-                if (fileInfo.Extension.Equals(".jpg") || fileInfo.Extension.Equals(".jpeg"))
-                {
-                    FileStream fs = new FileStream(LoadExifPath, FileMode.Open);
-
-                    BitmapSource img = BitmapFrame.Create(fs);
-
-                    BitmapMetadata md = (BitmapMetadata)img.Metadata;
-                    //
-                    //manufacturer from EXIF
-                    if (string.IsNullOrEmpty(md.CameraManufacturer))
-                        manufacturer = "Data not found";
-                    else
-                        manufacturer = md.CameraManufacturer;
-
-                    //
-                    //modelOfCamera from EXIF
-                    if (string.IsNullOrEmpty(md.CameraModel))
-                        modelOfCamera = "Data not found";
-                    else
-                        modelOfCamera = md.CameraModel;
-
-                    //
-                    //DateCreation from EXIF
-                    if (string.IsNullOrEmpty(md.DateTaken))
-                        dateCreation = "Data not found";
-                    else
-                        dateCreation = md.DateTaken;
-
-                    fs.Close();
-                }
-                else
-                {
-                    manufacturer = "Data not found";
-                    modelOfCamera = "Data not found";
-                    dateCreation = "Data not found";
-                }
-
-                //
-                //title from FileInfo
-                if (string.IsNullOrEmpty(fileInfo.Name))
-                    title = "Data not found";
-                else
-                    title = fileInfo.Name;
-
-                //
-                //FileSize from FileInfo
-                if (fileInfo.Length >= 1024)
-                {
-                    fileSize = Math.Round((fileInfo.Length / 1024f), 1).ToString() + " KB";
-                    if ((fileInfo.Length / 1024f) >= 1024f)
-                        fileSize = Math.Round((fileInfo.Length / 1024f) / 1024f, 2).ToString() + " MB";
-                }
-                else
-                    fileSize = fileInfo.Length.ToString() + " B";
-
-
-                //
-                //DateUpload from FileInfo
-                if (fileInfo.CreationTime == null)
-                    dateUpload = "Data not found";
-                else
-                    dateUpload = fileInfo.CreationTime.ToString("dd.MM.yyyy HH:mm:ss");
-
-               
-            }
-            catch(Exception err)
-            {
-                
-                // need to static errors
-            }
        
-            
-        }
-        
-
-        
         [HttpPost]
         public ActionResult Delete(string fileToDelete)
         {
@@ -194,7 +25,7 @@ namespace Gallery.Controllers
                 {
                     string dirHashName = fileToDelete.Replace(pathToSave, "").Replace(Path.GetFileName(fileToDelete), "").Replace("/", "");
 
-                    if (dirHashName == ComputeSha256Hash(User.Identity.Name))
+                    if (dirHashName == Servises.ComputeSha256Hash(User.Identity.Name))
                     {
                         if (Directory.Exists(Server.MapPath(fileToDelete.Replace(Path.GetFileName(fileToDelete), ""))))
                             System.IO.File.Delete(Server.MapPath(fileToDelete));
@@ -224,18 +55,6 @@ namespace Gallery.Controllers
             return RedirectToAction("Index");
         }
 
-
-        public ActionResult Index()
-        {
-            //System.Windows.MessageBox.Show(pathToSave);
-            return View();
-        }
-        public ActionResult Error()
-        {
-            return View();
-        }
-
-
         [HttpPost]
         public ActionResult Upload(HttpPostedFileBase files)
         {
@@ -256,7 +75,7 @@ namespace Gallery.Controllers
                                 bool IsLoad = true;
 
                                 // Encrypted User's directory path
-                                string DirPath = Server.MapPath(pathToSave) + ComputeSha256Hash(User.Identity.Name);
+                                string DirPath = Server.MapPath(pathToSave) + Servises.ComputeSha256Hash(User.Identity.Name);
 
                                 // extract only the filename
                                 var fileName = Path.GetFileName(files.FileName);
@@ -299,7 +118,7 @@ namespace Gallery.Controllers
 
                                                 CheckFileStream.Close();
                                                 
-                                                if (CompareBitmapsFast(TempBmp, CheckBmp))
+                                                if (Servises.CompareBitmapsFast(TempBmp, CheckBmp))
                                                 {
                                                     IsLoad = false;
                                                     ViewBag.Error = "Photo already exists!";
@@ -373,6 +192,18 @@ namespace Gallery.Controllers
             }
             return RedirectToAction("Index");
         }
+
+
+        public ActionResult Index()
+        {
+            //System.Windows.MessageBox.Show(pathToSave);
+            return View();
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+
 
         public ActionResult Upload()
         {
