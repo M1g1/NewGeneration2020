0ed4d57c5a9ca3cdee99cff6b9fcfacb0978b91c
M1g1 <migi4ok@gmail.com>
2020-02-21 16:56:47 +0200

    global.asax has been removed, logic moved to startup.cs

--- Gallery/Startup.cs
+++ Gallery/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 
 [assembly: OwinStartupAttribute(typeof(Gallery.Startup))]
 namespace Gallery
@@ -8,6 +11,10 @@ namespace Gallery
     {
         public void Configuration(IAppBuilder app)
         {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             ConfigureAuth(app);
         }
     }
