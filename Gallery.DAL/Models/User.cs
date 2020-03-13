e2547f9472b8c3bc70b80f44cbe71ef2bf8744f9
M1g1 <migi4ok@gmail.com>
2020-03-13 09:45:09 +0200

    Removed the temporary class. User model created.

--- Gallery.DAL/Models/User.cs
+++ Gallery.DAL/Models/User.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gallery.DAL
+namespace Gallery.DAL.Models
 {
-    public class Temporary
+    public class User
     {
         public int Id { get; private set; }
-
-        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
     }
-
 }
