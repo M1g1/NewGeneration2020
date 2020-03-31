343f72b1b6f2e4b4e2a7f755f94a40cb5b543263
M1g1 <migi4ok@gmail.com>
2020-03-31 12:25:41 +0300

    Added filter [EmailAddress] for login and registration models.

--- Gallery.DAL/Models/LoginModel.cs
+++ Gallery.DAL/Models/LoginModel.cs
@@ -5,7 +5,9 @@ namespace Gallery.DAL.Models
     public class LoginModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
--- Gallery.DAL/Models/RegisterModel.cs
+++ Gallery.DAL/Models/RegisterModel.cs
@@ -5,6 +5,7 @@ namespace Gallery.DAL.Models
     public class RegisterModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
