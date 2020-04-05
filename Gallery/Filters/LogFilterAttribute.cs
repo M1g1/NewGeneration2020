adde6c1caa8e336f020461c95220e8f9132837dc
M1g1 <migi4ok@gmail.com>
2020-04-05 17:41:01 +0300

    LogFilter implements exception logging. The http request now logs UserAgent instead of the browser.

--- Gallery/Filters/LogFilterAttribute.cs
+++ Gallery/Filters/LogFilterAttribute.cs
@@ -3,7 +3,7 @@ using NLog;
 
 namespace Gallery.Filters
 {
-    public class LogFilterAttribute : ActionFilterAttribute
+    public class LogFilterAttribute : ActionFilterAttribute, IExceptionFilter
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         public override void OnActionExecuting(ActionExecutingContext filterContext)
@@ -13,10 +13,10 @@ namespace Gallery.Filters
             var machineName = filterContext.HttpContext.Server.MachineName;
             var dnsName = request.UserHostName;
             var rawUrl = request.RawUrl;
-            var browser = request.Browser.Browser;
+            var userAgent = request.UserAgent;
             var httpMethod = request.HttpMethod;
             string messageRequest = "\n{\n\tRequest\n\tIP: " + ipAddress + "\n\tDNS-name: " + dnsName + "\n\tMachineName: " +
-                                    machineName + "\n\tURL-address: " + rawUrl + "\n\tBrowser: " + browser +
+                                    machineName + "\n\tURL-address: " + rawUrl + "\n\tUserAgent: " + userAgent +
                                     "\n\tHttpMethod: " + httpMethod + "\n}";
             Logger.Info(messageRequest);
             base.OnActionExecuting(filterContext);
@@ -29,6 +29,16 @@ namespace Gallery.Filters
             string messageResponse = "\n{\n\tResponse" + "\n\tStatusCode: " + status + "\n}";
             Logger.Info(messageResponse);
             base.OnActionExecuted(filterContext);
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (!filterContext.ExceptionHandled)
+            {
+                filterContext.ExceptionHandled = true;
+            }
+            var exception = filterContext.Exception;
+            Logger.Error(exception);
         }
     }
 }
