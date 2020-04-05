f9017bf3abaf98ebed76b4ed931dd383e04d4a00
M1g1 <migi4ok@gmail.com>
2020-04-05 15:23:59 +0300

    LogFilter implements http request logging.

--- Gallery/Filters/LogFilterAttribute.cs
+++ Gallery/Filters/LogFilterAttribute.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Mvc;
+using NLog;
 
 namespace Gallery.Filters
 {
-    public class LogFilterAttribute
+    public class LogFilterAttribute : ActionFilterAttribute
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            
+            var request = filterContext.HttpContext.Request;
+            var ipAddress = request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? request.UserHostAddress;
+            var machineName = filterContext.HttpContext.Server.MachineName;
+            var dnsName = request.UserHostName;
+            var rawUrl = request.RawUrl;
+            var browser = request.Browser.Browser;
+            var httpMethod = request.HttpMethod;
+            string messageRequest = "\n{\n\tRequest\n\tIP: " + ipAddress + "\n\tDNS-name: " + dnsName + "\n\tMachineName: " +
+                                    machineName + "\n\tURL-address: " + rawUrl + "\n\tBrowser: " + browser +
+                                    "\n\tHttpMethod: " + httpMethod + "\n}";
+            Logger.Info(messageRequest);
+            base.OnActionExecuting(filterContext);
+        }
     }
 }
