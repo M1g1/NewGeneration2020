8a8e3407c69ccc531e4abec22aea7ec9139f4f59
M1g1 <migi4ok@gmail.com>
2020-05-01 22:55:26 +0300

    Added constructor for migrations

--- Gallery.DAL/Models/UserContext.cs
+++ Gallery.DAL/Models/UserContext.cs
@@ -4,6 +4,7 @@ namespace Gallery.DAL.Models
 {
     public class UserContext : DbContext
     {
+        public UserContext() { }
         public UserContext(string nameOrConnectionString) : base(nameOrConnectionString)
         { }
         public DbSet<User> Users { get; set; }
