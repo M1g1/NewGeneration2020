e5a2c903a80ecb97fa4c5e8cd056a31b75415699
M1g1 <migi4ok@gmail.com>
2020-05-01 20:25:55 +0300

    Removed value types that can be null.

--- Gallery.DAL/Models/Media.cs
+++ Gallery.DAL/Models/Media.cs
@@ -5,10 +5,10 @@
         public int Id { get; set; }
         public string PathToMedia { get; set; }
 
-        public int? UserId { get; set; }
+        public int UserId { get; set; }
         public User User { get; set; }
 
-        public int? MediaTypeId { get; set; }
+        public int MediaTypeId { get; set; }
         public MediaType Type { get; set; }
     }
 }
