9fe660d2409abf8cf9085df217253a10ba271b19
M1g1 <migi4ok@gmail.com>
2020-05-01 14:23:36 +0300

    A list of roles has been added to the user model.

--- Gallery.DAL/Models/User.cs
+++ Gallery.DAL/Models/User.cs
@@ -1,9 +1,17 @@
-﻿namespace Gallery.DAL.Models
+﻿using System.Collections.Generic;
+
+namespace Gallery.DAL.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public List<Role> Roles { get; set; }
+
+        public User()
+        {
+            Roles = new List<Role>();
+        }
     }
 }
