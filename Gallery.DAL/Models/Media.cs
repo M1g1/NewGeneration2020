ecc305d56d7ad443bf706a53c9d9cb3cc591f547
M1g1 <migi4ok@gmail.com>
2020-05-01 17:25:29 +0300

    In the media model, the Name field is renamed to PathToMedia, fields for the media type are added.

--- Gallery.DAL/Models/Media.cs
+++ Gallery.DAL/Models/Media.cs
@@ -3,10 +3,12 @@
     public class Media
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string PathToMedia { get; set; }
 
         public int? UserId { get; set; }
         public User User { get; set; }
 
+        public int? MediaTypeId { get; set; }
+        public MediaType Type { get; set; }
     }
 }
