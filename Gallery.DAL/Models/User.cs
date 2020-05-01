0d3a273274bc3191a9fb7ae21d5ce088cc4969f2
M1g1 <migi4ok@gmail.com>
2020-05-01 16:29:06 +0300

    A media model is added to the Entity Framework context. A list of media has been added to the user model.

--- Gallery.DAL/Models/User.cs
+++ Gallery.DAL/Models/User.cs
@@ -8,10 +8,12 @@ namespace Gallery.DAL.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public List<Role> Roles { get; set; }
+        public List<Media> Media { get; set; }
 
         public User()
         {
             Roles = new List<Role>();
+            Media = new List<Media>();
         }
     }
 }
