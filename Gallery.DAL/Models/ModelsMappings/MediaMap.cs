a67ecee59eb6728990882cbb20592348f6f3bcc0
M1g1 <migi4ok@gmail.com>
2020-05-06 22:40:20 +0300

    Added files to configure models mappings.

---
+++ Gallery.DAL/Models/ModelsMappings/MediaMap.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace Gallery.DAL.Models.ModelsMappings
+{
+    public class MediaMap: EntityTypeConfiguration<Media>
+    {
+        public MediaMap()
+        {
+            
+        }
+    }
+}
---
+++ Gallery.DAL/Models/ModelsMappings/MediaTypeMap.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace Gallery.DAL.Models.ModelsMappings
+{
+    public class MediaTypeMap: EntityTypeConfiguration<MediaType>
+    {
+        public MediaTypeMap()
+        {
+            
+        }
+    }
+}
