6a7de59514740f4a91432595b909afa219ba7d1d
M1g1 <migi4ok@gmail.com>
2020-05-07 05:03:52 +0300

    Added migration files with schema change.

---
+++ Gallery.DAL/Migrations/202005070152597_AddNewDefaultSchema.cs
@@ -0,0 +1,26 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNewDefaultSchema : DbMigration
+    {
+        public override void Up()
+        {
+            MoveTable(name: "dbo.Media", newSchema: "Gallery");
+            MoveTable(name: "dbo.MediaTypes", newSchema: "Gallery");
+            MoveTable(name: "dbo.Users", newSchema: "Gallery");
+            MoveTable(name: "dbo.Roles", newSchema: "Gallery");
+            MoveTable(name: "dbo.RolesUsers", newSchema: "Gallery");
+        }
+        
+        public override void Down()
+        {
+            MoveTable(name: "Gallery.RolesUsers", newSchema: "dbo");
+            MoveTable(name: "Gallery.Roles", newSchema: "dbo");
+            MoveTable(name: "Gallery.Users", newSchema: "dbo");
+            MoveTable(name: "Gallery.MediaTypes", newSchema: "dbo");
+            MoveTable(name: "Gallery.Media", newSchema: "dbo");
+        }
+    }
+}
