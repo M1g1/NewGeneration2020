68d0350b60abba951d76848c35edc45a4783f0ac
M1g1 <migi4ok@gmail.com>
2020-05-07 23:29:56 +0300

    Added migration files with the addition of a new Attempts table.

---
+++ Gallery.DAL/Migrations/202005072006373_AddAttemptsTable.cs
@@ -0,0 +1,33 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAttemptsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "Gallery.Attempts",
+                c => new
+                    {
+                        AttemptId = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        IsSuccess = c.Boolean(nullable: false),
+                        IpAddress = c.String(nullable: false, maxLength: 64),
+                        TimeStamp = c.DateTime(nullable: false, precision: 7, storeType: "datetime2"),
+                    })
+                .PrimaryKey(t => t.AttemptId)
+                .ForeignKey("Gallery.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("Gallery.Attempts", "UserId", "Gallery.Users");
+            DropIndex("Gallery.Attempts", new[] { "UserId" });
+            DropTable("Gallery.Attempts");
+        }
+    }
+}
