ce93f9a54186217265e4704687c6171f37c0cb55
M1g1 <migi4ok@gmail.com>
2020-05-07 23:53:16 +0300

    Added migration files with changing field name

---
+++ Gallery.DAL/Migrations/202005072051413_FieldNameChanged.cs
@@ -0,0 +1,20 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FieldNameChanged : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("Gallery.Media", "Path", c => c.String(nullable: false));
+            DropColumn("Gallery.Media", "PathToMedia");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("Gallery.Media", "PathToMedia", c => c.String(nullable: false));
+            DropColumn("Gallery.Media", "Path");
+        }
+    }
+}
