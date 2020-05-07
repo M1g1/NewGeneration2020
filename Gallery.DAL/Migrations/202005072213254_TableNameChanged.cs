13ceda9c67f624593529bd5b8ff8befdfaf4777a
M1g1 <migi4ok@gmail.com>
2020-05-08 01:15:30 +0300

    Added migration files with table name change

---
+++ Gallery.DAL/Migrations/202005072213254_TableNameChanged.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TableNameChanged : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "Gallery.Attempts", newName: "LoginAttempts");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "Gallery.LoginAttempts", newName: "Attempts");
+        }
+    }
+}
