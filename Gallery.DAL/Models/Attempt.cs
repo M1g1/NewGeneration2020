626106c905723b4e2a7611c402aaf59f6d532b48
M1g1 <migi4ok@gmail.com>
2020-05-07 23:21:37 +0300

    Login Attempt Model Created

---
+++ Gallery.DAL/Models/Attempt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gallery.DAL.Models
+{
+    public class Attempt
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public bool IsSuccess { get; set; }
+        public string IpAddress { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
