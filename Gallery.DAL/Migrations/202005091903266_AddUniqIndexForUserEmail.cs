24c67c5d0d869281f079bc2eabdceb693541b8e5
M1g1 <migi4ok@gmail.com>
2020-05-09 22:12:03 +0300

    Added migration files AddUniqIndexForUserEmail

---
+++ Gallery.DAL/Migrations/202005091903266_AddUniqIndexForUserEmail.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUniqIndexForUserEmail : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("Gallery.Users", "Email", unique: true);
+        }
+        
+        public override void Down()
+        {
+            DropIndex("Gallery.Users", new[] { "Email" });
+        }
+    }
+}
