c733b4d8dbbf83352ce5db98c2e861897706fb35
M1g1 <migi4ok@gmail.com>
2020-05-14 19:10:38 +0300

    Tests changed for mocked file system.

--- Tests/FileStorageProvider.NUnitTests/MediaStorageProviderTests.cs
+++ Tests/FileStorageProvider.NUnitTests/MediaStorageProviderTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.IO.Abstractions.TestingHelpers;
 using FileStorageProvider.Interfaces;
 using FileStorageProvider.Providers;
 using NUnit.Framework;
@@ -8,67 +9,70 @@ namespace FileStorageProvider.NUnitTests
 {
     public class MediaStorageProviderTests
     {
+        private MockFileSystem _mockFileSystem;
+        private IFileStorage _mediaStorage;
+
         [SetUp]
         public void Setup()
         {
+            _mockFileSystem = new MockFileSystem();
+            _mockFileSystem.AddDirectory("test");
+            _mediaStorage = new MediaStorageProvider(_mockFileSystem);
         }
 
         [Test]
         public void Save_ArgumentPathIsNull_ThrowArgumentNullException()
         {
-            IFileStorage mediaStorage = new MediaStorageProvider();
-
             byte[] content = new byte[] { };
+
             string path = null;
 
-            Assert.Throws<ArgumentNullException>(() => mediaStorage.Save(content, path));
+            Assert.Throws<ArgumentNullException>(() => _mediaStorage.Save(content, path));
         }
 
         [Test]
         public void Save_ArgumentPathIsEmptyOrWhiteSpace_ThrowArgumentException()
         {
-            IFileStorage mediaStorage = new MediaStorageProvider();
-
             byte[] content = new byte[] { };
+
             string path = " ";
 
-            Assert.Throws<ArgumentException>(() => mediaStorage.Save(content, path));
+            Assert.Throws<ArgumentException>(() => _mediaStorage.Save(content, path));
 
         }
 
         [Test]
         public void Save_ArgumentContentIsNull_ThrowArgumentException()
         {
-            IFileStorage mediaStorage = new MediaStorageProvider();
-
             byte[] content = null;
-            string path = "C:\\Users\\User\\Desktop\\MySite\\NewGeneration2020\\Gallery\\Pictures\\Images\\File.png";
 
-            Assert.Throws<ArgumentNullException>(() => mediaStorage.Save(content, path));
+            string path = @"C:\test\test.png";
+
+            Assert.Throws<ArgumentNullException>(() => _mediaStorage.Save(content, path));
 
         }
 
         [Test]
         public void Save_ArgumentPathNonExistentDirectory_ThrowDirectoryNotFoundException()
         {
-            IFileStorage mediaStorage = new MediaStorageProvider();
-
+            
             byte[] content = new byte[] { };
-            string path = "C:\\Users\\User\\Desktop\\MySitesr\\p.png";
 
-            Assert.Throws<DirectoryNotFoundException>(() => mediaStorage.Save(content, path));
+            string path = @"C:\Desktop\test.png";
+
+            Assert.Throws<DirectoryNotFoundException>(() => _mediaStorage.Save(content, path));
 
         }
 
         [Test]
         public void Save_ArgumentsIsCorrect_ReturnTrue()
         {
-            IFileStorage mediaStorage = new MediaStorageProvider();
 
             byte[] content = new byte[] { };
-            string path = "C:\\Users\\User\\Desktop\\MySite\\NewGeneration2020\\Gallery\\Pictures\\Images\\File.png";
 
-            var result = mediaStorage.Save(content, path);
+            string path = @"C:\test\test.png";
+
+            var result = _mediaStorage.Save(content, path);
 
             Assert.AreEqual(true, result);
 
