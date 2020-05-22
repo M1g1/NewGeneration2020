0341836e78e3c743a866304a156653bee8a6093d
M1g1 <migi4ok@gmail.com>
2020-05-22 14:18:32 +0300

    Added a new project for testing Gallery.DAL

---
+++ Tests/Gallery.DAL.NUnitTests/MediaRepositoryTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Gallery.DAL.NUnitTests
+{
+    public class MediaRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+  
+    }
+}
