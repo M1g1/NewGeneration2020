3cacf803f400305fe2826c118658935ec0ce4a05
M1g1 <migi4ok@gmail.com>
2020-05-22 14:30:36 +0300

    Added a new project for testing Gallery.Service

---
+++ Tests/Gallery.Service.NUnitTests/ImageServiceTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Gallery.Service.NUnitTests
+{
+    public class ImageServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+      
+    }
+}
