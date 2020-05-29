52c4f60283d8001d2f1e4e2a3c74b90b1bbb09ed
M1g1 <migi4ok@gmail.com>
2020-05-29 15:41:48 +0300

    Data transfer object created for media upload attempts

---
+++ Gallery.Service/Contract/MediaUploadAttemptDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gallery.Service.Contract
+{
+    public class MediaUploadAttemptDto
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public int UserId { get; set; }
+        public bool IsInProgress { get; set; }
+        public bool IsSuccess { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
