2bd330c2e05db7b67679bf199719e5465e2f93af
M1g1 <migi4ok@gmail.com>
2020-05-29 17:19:51 +0300

    Migration files added
    -added new IpAddress field for MediaUploadAttempts table

---
+++ Gallery.DAL/Migrations/202005291410416_AddIpAddressFieldToMediaUploadAttempts.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIpAddressFieldToMediaUploadAttempts : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("Gallery.MediaUploadAttempts", "IpAddress", c => c.String(nullable: false, maxLength: 64));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("Gallery.MediaUploadAttempts", "IpAddress");
+        }
+    }
+}
