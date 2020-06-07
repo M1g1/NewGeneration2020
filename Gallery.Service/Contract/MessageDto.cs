36563678fa151136f2470c9c887869a7ad181018
M1g1 <migi4ok@gmail.com>
2020-06-07 23:58:52 +0300

    Message data transfer object created for message queue

---
+++ Gallery.Service/Contract/MessageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Gallery.Service.Contract
+{
+    public class MessageDto
+    {
+        public int UserId;
+        public string Path;
+        public string TempPath;
+        public byte[] MediaBytes;
+    }
+}
