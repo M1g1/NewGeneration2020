514eb0216f67478c8c1d7e7779f09faaa763a648
M1g1 <migi4ok@gmail.com>
2020-06-12 03:55:56 +0300

    Created Di config for Gallery.Worker using Autofac

---
+++ Gallery.Worker/App_Start/Modules/WorksModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Gallery.Worker.Works;
+
+namespace Gallery.App_Start.Modules
+{
+    public class WorksModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<UploadImageWork>().AsSelf();
+        }
+    }
+}
