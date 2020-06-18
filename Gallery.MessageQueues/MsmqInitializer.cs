843c71bd50416208ed76735903118b0a44280dde
M1g1 <migi4ok@gmail.com>
2020-06-19 02:13:10 +0300

    msmq initializer was created and method of creating queues if they not exist was implemented

---
+++ Gallery.MessageQueues/MsmqInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Messaging;
+
+namespace Gallery.MessageQueues
+{
+    public static class MsmqInitializer
+    {
+        public static void CreateIfNotExist(string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                if (!MessageQueue.Exists(path))
+                {
+                    MessageQueue.Create(path);
+                }
+            }
+        }
+    }
+}
