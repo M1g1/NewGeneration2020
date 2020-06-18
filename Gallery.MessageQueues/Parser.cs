600653309e5f00dd78e4069d8a25577748af328b
M1g1 <migi4ok@gmail.com>
2020-06-19 02:09:48 +0300

    Parser was created and method for getting microsoft queues paths was implemented

---
+++ Gallery.MessageQueues/Parser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Gallery.Config.Manager;
+
+namespace Gallery.MessageQueues
+{
+    public static class Parser
+    {
+        public static string[] ParseQueuePaths()
+        {
+            var queuePaths = GalleryConfigurationManager.GetMsmqPaths();
+            var separator = new [] {","};
+            return queuePaths.Split(separator, StringSplitOptions.RemoveEmptyEntries)
+                .Select(e => e.Trim()).ToArray();
+        }
+    }
+}
