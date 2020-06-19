30ad8b20e6d53e95661a12821e901849725266fc
M1g1 <migi4ok@gmail.com>
2020-06-19 14:32:51 +0300

    Classes for serialization and deserialization created and implemented

---
+++ Gallery.MessageQueues/Deserializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace Gallery.MessageQueues
+{
+    public class Deserializer
+    {
+        public static T DeserializeToObject<T>(string obj) where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(obj);
+        }
+        public static string DeserializeToString(byte[] obj)
+        {
+            return Encoding.UTF8.GetString(obj);
+        }
+    }
+}
---
+++ Gallery.MessageQueues/Serializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace Gallery.MessageQueues
+{
+    public class Serializer
+    {
+        public static string SerializeToJson<T>(T obj) where T : class
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static byte[] SerializeToBytes(string obj)
+        {
+            return Encoding.UTF8.GetBytes(obj);
+        }
+    }
+}
