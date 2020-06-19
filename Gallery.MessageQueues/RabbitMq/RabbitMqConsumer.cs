9bce09c02b024bee847a68cdad8ffe2a7ba48ff7
M1g1 <migi4ok@gmail.com>
2020-06-19 14:57:01 +0300

    RabbitMq consumer and publisher created

---
+++ Gallery.MessageQueues/RabbitMq/RabbitMqConsumer.cs
@@ -0,0 +1,10 @@
+﻿namespace Gallery.MessageQueues.RabbitMq
+{
+    public class RabbitMqConsumer : IConsumer
+    {
+        public T GetFirstMessage<T>(string messageQueuePath) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Gallery.MessageQueues/RabbitMq/RabbitMqPublisher.cs
@@ -0,0 +1,10 @@
+﻿namespace Gallery.MessageQueues.RabbitMq
+{
+    public class RabbitMqPublisher : IPublisher
+    {
+        public void SendMessage<T>(T message, string queuePath) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
