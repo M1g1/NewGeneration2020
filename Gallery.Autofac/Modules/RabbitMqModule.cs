7b289f6ca9b77fdbf6259b3296884ff2c2119318
M1g1 <migi4ok@gmail.com>
2020-06-22 00:41:24 +0300

    For rabbitmq created: rabbitmqparser, autofac rabbitmqmodule
    -rabbitmq dependencies registered in rabbitmq module

---
+++ Gallery.Autofac/Modules/RabbitMqModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Gallery.Config.Manager;
+using Gallery.MessageQueues;
+using Gallery.MessageQueues.RabbitMq;
+
+namespace Gallery.Autofac.Modules
+{
+    public class RabbitMqModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            var cs = GalleryConfigurationManager.GetRabbitMqConnectionString();
+
+            containerBuilder.Register(p => new RabbitMqParser()).AsSelf().As<IQueueParser>();
+
+            containerBuilder.Register(init => new RabbitMqInitializer(cs)).AsSelf().As<IQueueInitialize>();
+
+            var rabbitMqInitializer = new RabbitMqInitializer(cs);
+
+            var rabbitmqParser = new RabbitMqParser();
+
+            rabbitMqInitializer.CreateIfNotExist(rabbitmqParser.ParseQueuePaths());
+
+            containerBuilder.Register(pub => new RabbitMqPublisher(cs)).AsSelf().As<IPublisher>();
+
+            containerBuilder.Register(cons => new RabbitMqConsumer(cs)).AsSelf().As<IConsumer>();
+        }
+    }
+}
---
+++ Gallery.MessageQueues/RabbitMq/RabbitMqParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Gallery.Config.Manager;
+
+namespace Gallery.MessageQueues.RabbitMq
+{
+    public class RabbitMqParser : IQueueParser
+    {
+        public string[] ParseQueuePaths()
+        {
+            var queuePaths = GalleryConfigurationManager.GetRabbitMqPaths();
+            var separator = new [] {","};
+            return queuePaths.Split(separator, StringSplitOptions.RemoveEmptyEntries)
+                .Select(e => e.Trim()).ToArray();
+        }
+    }
+}
