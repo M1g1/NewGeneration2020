bdd8c33d8db0f3a52fcadadf6f4038fe1cf13e91
M1g1 <migi4ok@gmail.com>
2020-06-22 13:54:04 +0300

    Implemented rabbitmq publisher and consumer

--- Gallery.MessageQueues/RabbitMq/RabbitMqConsumer.cs
+++ Gallery.MessageQueues/RabbitMq/RabbitMqConsumer.cs
@@ -1,10 +1,47 @@
-﻿namespace Gallery.MessageQueues.RabbitMq
+﻿using System;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Gallery.MessageQueues.RabbitMq
 {
     public class RabbitMqConsumer : IConsumer
     {
+        private readonly string _connectionString;
+        private readonly EventWaitHandle _waitHandle = new AutoResetEvent(false);
+
+        public RabbitMqConsumer(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
         public T GetFirstMessage<T>(string messageQueuePath) where T : class
         {
-            throw new System.NotImplementedException();
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(_connectionString)
+            };
+            using (var connection = factory.CreateConnection())
+            using (var model = connection.CreateModel())
+            {
+                T message = default(T);
+
+                var consumer = new EventingBasicConsumer(model);
+
+                consumer.Received += (eventModel, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    message = Deserializer.DeserializeToObject<T>(Deserializer.DeserializeToString(obj: body));
+                    _waitHandle.Set();
+                };
+                model.BasicConsume(
+                    queue: messageQueuePath,
+                    autoAck: true,
+                    consumer: consumer);
+
+                _waitHandle.WaitOne();
+                return message;
+            }
         }
     }
 }
--- Gallery.MessageQueues/RabbitMq/RabbitMqPublisher.cs
+++ Gallery.MessageQueues/RabbitMq/RabbitMqPublisher.cs
@@ -1,10 +1,30 @@
-﻿namespace Gallery.MessageQueues.RabbitMq
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Gallery.MessageQueues.RabbitMq
 {
     public class RabbitMqPublisher : IPublisher
     {
+        private readonly string _connectionString;
+
+        public RabbitMqPublisher(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
         public void SendMessage<T>(T message, string queuePath) where T : class
         {
-            throw new System.NotImplementedException();
+            
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(_connectionString)
+            };
+            using (var connection = factory.CreateConnection())
+            using (var model = connection.CreateModel())
+            {
+                var messageBody = Serializer.SerializeToBytes(Serializer.SerializeToJson<T>(message));
+
+                model.BasicPublish(string.Empty, queuePath, body: messageBody);
+            }
         }
     }
 }
