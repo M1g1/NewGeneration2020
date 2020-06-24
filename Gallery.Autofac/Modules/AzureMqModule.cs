2ee1716fad7d79ca0ecf76a2cfbdbda2acaf1ab2
M1g1 <migi4ok@gmail.com>
2020-06-24 03:25:01 +0300

    Autofac module for registering dependencies Azure Storage Queue created

---
+++ Gallery.Autofac/Modules/AzureMqModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Gallery.Config.Manager;
+using Gallery.MessageQueues;
+using Gallery.MessageQueues.AzureStorageQueue;
+
+namespace Gallery.Autofac.Modules
+{
+    public class AzureMqModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            var cs = GalleryConfigurationManager.GetAzureMqConnectionString();
+
+            containerBuilder.Register(p => new AzureStorageQueueParser()).AsSelf().As<IQueueParser>();
+
+            containerBuilder.Register(init => new AzureStorageQueueInitializer(cs)).AsSelf().As<IQueueInitialize>();
+
+            var azureMqInitializer = new AzureStorageQueueInitializer(cs);
+
+            var azureMqParser = new AzureStorageQueueParser();
+
+            azureMqInitializer.CreateIfNotExist(azureMqParser.ParseQueuePaths());
+
+            containerBuilder.Register(pub => new AzureStorageQueuePublisher(cs)).AsSelf().As<IPublisher>();
+
+            containerBuilder.Register(cons => new AzureStorageQueueConsumer(cs)).AsSelf().As<IConsumer>();
+        }
+    }
+}
