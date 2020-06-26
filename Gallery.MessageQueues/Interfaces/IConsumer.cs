dd64a7752c01a375196e757908026f9e96caa24a
M1g1 <migi4ok@gmail.com>
2020-06-26 15:16:02 +0300

    Added xml documentation to IConsumer and IPublisher

--- Gallery.MessageQueues/Interfaces/IConsumer.cs
+++ Gallery.MessageQueues/Interfaces/IConsumer.cs
@@ -2,6 +2,15 @@
 {
     public interface IConsumer
     {
+        /// <summary>
+        /// The method expects messages if they are not in the queue,
+        /// and receives the first message from the queue, if any.
+        /// </summary>
+        /// <typeparam name="T">Return message type.</typeparam>
+        /// <param name="queueName">The name of the queue.</param>
+        /// <returns>
+        /// Returns first message from queue deserialized to type <typeparamref name="T"/>.
+        /// </returns>
         T GetFirstMessage<T>(string queueName) where T : class;
     }
 }
--- Gallery.MessageQueues/Interfaces/IPublisher.cs
+++ Gallery.MessageQueues/Interfaces/IPublisher.cs
@@ -2,6 +2,12 @@
 {
     public interface IPublisher
     {
+        /// <summary>
+        /// Send message to queue.
+        /// </summary>
+        /// <typeparam name="T">Type of message to send.</typeparam>
+        /// <param name="message">Message to send.</param>
+        /// <param name="queueName">The name of the queue.</param>
         void SendMessage<T>(T message, string queueName) where T : class;
     }
 }
