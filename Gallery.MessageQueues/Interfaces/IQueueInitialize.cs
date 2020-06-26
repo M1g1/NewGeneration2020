d1d08b6be8635b308f5a76e2b885537dfc146d81
M1g1 <migi4ok@gmail.com>
2020-06-26 16:37:43 +0300

    Added xml documentation to IQueueInitialize

--- Gallery.MessageQueues/Interfaces/IQueueInitialize.cs
+++ Gallery.MessageQueues/Interfaces/IQueueInitialize.cs
@@ -2,6 +2,10 @@
 {
     public interface IQueueInitialize
     {
+        /// <summary>
+        /// Create queue if not exist.
+        /// </summary>
+        /// <param name="queueName">Array of queue names</param>
         void CreateIfNotExist(string queueName);
     }
 }
