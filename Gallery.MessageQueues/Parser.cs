e093b5b5dffff446fc9bb1a6f82c484c57f1c3c4
M1g1 <migi4ok@gmail.com>
2020-06-30 23:26:12 +0300

    Implemented a static parser

--- Gallery.MessageQueues/Parser.cs
+++ Gallery.MessageQueues/Parser.cs
@@ -5,6 +5,19 @@ namespace Gallery.MessageQueues
 {
     public static class Parser
     {
-        
+        public static IDictionary<QueueType, string> ParseQueueNames()
+        { 
+            var dictionary = new Dictionary<QueueType, string>();
+
+            var uploadImgvalue = GalleryConfigurationManager.GetUploadImageQueueName();
+
+            var uploadMp4value = GalleryConfigurationManager.GetUploadMp4QueueName();
+
+            dictionary.Add(QueueType.UploadImage, uploadImgvalue);
+
+            dictionary.Add(QueueType.UploadMp4, uploadMp4value);
+
+            return dictionary;
+        }
     }
 }
